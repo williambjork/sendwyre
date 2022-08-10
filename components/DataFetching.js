9b0bfaf33995d2a5fb00e104cbfc0469603fd981
williambjork <william.bjork@gobil.se>
2022-08-10 16:19:28 +0200

    sucessfull POST return from testwyre

--- components/DataFetching.js
+++ components/DataFetching.js
@@ -5,23 +5,29 @@ function DataFetching() {
 
     const [posts, setPosts] = useState([]);
 
+    const options = {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer TEST-SK-6C9AXWAX-YZ976B2W-TGD9B6R2-JD6ACY3P'
+        },
+        body: JSON.stringify({referrerAccountId: 'AC_PD7YFA9LEPA'})
+      };
+
     useEffect(() => {
-       axios.get('https://jsonplaceholder.typicode.com/posts') 
-       .then(res => {
-        console.log(res)
-        setPosts(res.data)
-       })
-       .catch(err => {
-        console.log(err)
-       })
+      fetch('https://api.testwyre.com/v3/orders/reserve', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
     }, [])
 
   return (
     <div>
         <ul>
-            {
+            {/*
                 posts.map(post => <li key={post.id}>{post.title}</li>)
-            }
+  */}
         </ul>
     </div>
   )
