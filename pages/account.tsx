824f46e39df803092ef89201027af077c534caf6
williambjork <william.bjork@gobil.se>
2022-08-23 13:01:18 +0200

    added sign out button

--- pages/account.tsx
+++ pages/account.tsx
@@ -16,30 +16,25 @@ function account() {
         className="
                    bg-[url(https://i.postimg.cc/nr89xSmc/gridgrid2opac2.png)] min-h-screen bg-cover flex w-full flex-3 flex-col items-center justify-center px-20 text-center"
       >
-        <a
-          className="relative block p-10 border border-gray-100 shadow-xl bg-white rounded-xl"
-          href=""
-        >
-          
-          
+        <div className="relative block items-center p-10 border border-gray-100 shadow-xl bg-white rounded-xl">
           <img
-            className="h-30 w-30  rounded-full absolute top-3 "
+            className="h-30 w-30 content-center rounded-full top-3 "
             src={session?.user?.image!}
             alt=""
           />
 
           <div className="mt-4 text-gray-500 sm:pr-8 pt-10">
-         
-
             <h5 className="mt-4 text-xl font-bold text-gray-900">
-            {session?.user?.name}
+              {session?.user?.name}
             </h5>
 
             <p className="hidden mt-2 text-sm sm:block">
-            {session?.user?.email}
+              {session?.user?.email}
             </p>
           </div>
-        </a>
+
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
       </main>
     </div>
   );
